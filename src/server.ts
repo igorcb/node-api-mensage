a7052ddf0d260a17a8cd1bf2b1f9f036ee23b5bd
Igor C. Batista <igor.batista@gmail.com>
2020-10-27 10:18:03 -0300

    config routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 
+import routes from './routes';
 
 import './database/connection';
 
 
 const app = express();
 
-app.get('/');
+app.use(express.json());
+app.use(routes);
 
-app.listen(3000);
+
+app.listen(3000, () => console.log(' Server started at http://localhost:3000'));
