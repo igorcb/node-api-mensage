8a32a750f6f9abacae941c7fe90ef046c9368895
Igor C. Batista <igor.batista@gmail.com>
2020-10-27 13:36:44 -0300

    config database .env

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,15 +1,29 @@
-import { ConnectionOptions } from 'typeorm';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import 'dotenv/config';
+
+const { POSTGRES_DB } = process.env;
+const { POSTGRES_HOST } = process.env;
+const { POSTGRES_PORT } = process.env;
+const { POSTGRES_USER } = process.env;
+const { POSTGRES_PASSWORD } = process.env;
  
-const ConnectionOptions = {
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  cli: {
-    migrationsDir: 'src/database/migrations',
-  }
-};
- 
-export default ConnectionOptions;
+createConnection({
+    type: "postgres",
+    host: POSTGRES_HOST,
+    port: Number(POSTGRES_PORT),
+    username: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_DB,   
+    migrations: [
+      "./src/database/migrations/*.ts"
+    ],
+    entities: [
+      "./src/models/*.ts"
+    ],    
+    cli: {
+      migrationsDir: 'src/database/migrations',
+    }   
+}).then(connection => {
+   console.log("Database connect sucessfull !!!" );
+}).catch(error => console.log(error));
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 
+import './database/connection';
 import routes from './routes';
 
-import './database/connection';
-
-
 const app = express();
+
 
 app.use(express.json());
 app.use(routes);
 
-
 app.listen(3000, () => console.log(' Server started at http://localhost:3000'));
+
